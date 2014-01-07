ea658a0c51efee7f4081015bcba9a6adfcb59a4c
dadadel <daouzli@gmail.com>
2014-01-07 15:07:32 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Pyment',
+      version='0.0.1',
+      description='',
+      author='A. Daouzli',
+      author_email='',
+      url='https://github.com/dadadel/pyment',
+      packages=['pyment'],
+     )
+
