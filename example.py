2b542122825091fba6c32aae428ef6e4cef728dc
dadadel <daouzli@gmail.com>
2014-03-25 23:05:43 +0100

    add numpydoc examples

--- example.py
+++ example.py
@@ -45,3 +45,59 @@ class SomeClass(object):
 
     def method2(self, prm1, prm2='defaultprm'):
         pass
+
+    def method_numpy(self):
+        """
+        My numpydoc description of a kind
+        of very exhautive numpydoc format docstring.
+
+        Parameters
+        ----------
+        first : array_like
+            the 1st param name `first`
+        second :
+            the 2nd param
+        third : {'value', 'other'}, optional
+            the 3rd param, by default 'value'
+
+        Returns
+        -------
+        string
+            a value in a string
+
+        Raises
+        ------
+        KeyError
+            when a key error
+        OtherError
+            when an other error
+
+        See Also
+        --------
+        a_func : linked (optional), with things to say
+                 on several lines
+        some blabla
+
+        Note
+        ----
+        Some informations.
+
+        Some maths also:
+        .. math:: f(x) = e^{- x}
+
+        References
+        ----------
+        Biblio with cited ref [1]_. The ref can be cited in Note section.
+
+        .. [1] Adel Daouzli, Sylvain Saïghi, Michelle Rudolph, Alain Destexhe,
+           Sylvie Renaud: Convergence in an Adaptive Neural Network:
+           The Influence of Noise Inputs Correlation. IWANN (1) 2009: 140-148
+
+        Examples
+        --------
+        This is example of use
+        >>> print "a"
+        a
+
+        """
+        pass
