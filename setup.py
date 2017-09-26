42e54e39449ab44ef36966f13698477e079bf332
Daouzli A <daouzli@gmail.com>
2017-09-26 22:48:31 +0200

    update setup

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from setuptools import setup
 from os import path
+import pyment
+
 
 curr_dir = path.abspath(path.dirname(__file__))
 
@@ -10,26 +12,26 @@ with open(path.join(curr_dir, "README.rst")) as f:
 
 
 setup(name='Pyment',
-      version='0.3.2-dev3',
-      description='Generate automatically the doc from your code signature',
+      version=pyment.__version__,
+      description='Generate/convert automatically the docstrings from code signature',
       long_description=long_desc,
       author='A. Daouzli',
-      author_email='',
+      author_email='dadel@hadoly.fr',
       license='GPLv3',
-      keywords="pyment numpydoc doc development generate auto",
-      classifiers = [
+      keywords="pyment docstring numpydoc googledoc restructuredtext epydoc epytext javadoc development generate auto",
+      classifiers=[
           'Intended Audience :: Developers',
           'Topic :: Documentation',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.0',
-	  'Programming Language :: Python :: 3.1',
-	  'Programming Language :: Python :: 3.2',
-	  'Programming Language :: Python :: 3.3',
-  	  'Programming Language :: Python :: 3.4',
- 	  'Programming Language :: Python :: 3.5',
-	  'Programming Language :: Python :: 3.6'
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
           ],
       url='https://github.com/dadadel/pyment',
       packages=['pyment'],
