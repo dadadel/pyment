8c8d23eaf00556d6ec945980917d40be2f01ffb1
Daouzli A <daouzli@gmail.com>
2018-07-25 21:34:28 +0200

    set name in __init__.py

--- pyment/__init__.py
+++ pyment/__init__.py
@@ -1,3 +1,5 @@
 from .pyment import PyComment, __version__, __copyright__, __author__, __licence__
 
+name = "pyment"
+
 __all__ = ['PyComment', '__version__', '__copyright__', '__author__', '__licence__']
