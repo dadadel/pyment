fd2e2f3d98b510c7217c64de45f6c772fca4d3c8
Peer Wagner <wagnerpeer@gmail.com>
2018-07-26 22:39:45 +0200

    Pulled methods to base class where exactly or almost identical

--- pyment/docstring.py
+++ pyment/docstring.py
@@ -92,6 +92,7 @@ class DocToolsBase(object):
                  optional_sections=None,
                  excluded_sections=None,
                  opt=None,
+                 section_headers=None,
                  ):
         """
 
@@ -104,11 +105,153 @@ class DocToolsBase(object):
         :param excluded_sections: list of sections that are excluded,
           even if mandatory. The list is the same as for optional sections.
         :type excluded_sections: list
+        :param opt:
+        :type opt:
+        :param section_headers:
+        :type section_headers:
         """
         self.first_line = first_line
         self.optional_sections = list(optional_sections)
         self.excluded_sections = list(excluded_sections)
         self.opt = opt
+        self.section_headers = section_headers
+
+    def __iter__(self):
+        return self.opt.__iter__()
+
+    def __getitem__(self, key):
+        return self.opt[key]
+
+    def get_optional_sections(self):
+        """Get optional sections"""
+        return self.optional_sections
+
+    def get_excluded_sections(self):
+        """Get excluded sections"""
+        return self.excluded_sections
+
+    def get_mandatory_sections(self):
+        """Get mandatory sections"""
+        return [s for s in self.opt
+                if s not in self.optional_sections and
+                   s not in self.excluded_sections]
+
+    def get_list_key(self, data, key):
+        """Get the list of a key elements.
+        Each element is a tuple (key=None, description, type=None).
+        Note that the tuple's element can differ depending on the key.
+
+        :param data: the data to proceed
+        :param key: the key
+
+        """
+        raise NotImplementedError
+
+    def get_raise_list(self, data):
+        """Get the list of exceptions.
+        The list contains tuples (name, desc)
+
+        :param data: the data to proceed
+
+        """
+        return_list = []
+        lst = self.get_list_key(data, 'raise')
+        for l in lst:
+            # assume raises are only a name and a description
+            name, desc, _ = l
+            return_list.append((name, desc))
+
+        return return_list
+
+    def get_return_list(self, data):
+        """Get the list of returned values.
+        The list contains tuples (name=None, desc, type=None)
+
+        :param data: the data to proceed
+
+        """
+        return_list = []
+        lst = self.get_list_key(data, 'return')
+        for l in lst:
+            name, desc, rtype = l
+            if l[2] is None:
+                rtype = l[0]
+                name = None
+                desc = desc.strip()
+            return_list.append((name, desc, rtype))
+
+        return return_list
+
+    def get_param_list(self, data):
+        """Get the list of parameters.
+        The list contains tuples (name, desc, type=None)
+
+        :param data: the data to proceed
+
+        """
+        return self.get_list_key(data, 'param')
+
+    def get_next_section_start_line(self, data):
+        """Get the starting line number of next section.
+        It will return -1 if no section was found.
+        The section is a section key (e.g. 'Parameters:')
+        then the content
+
+        :param data: a list of strings containing the docstring's lines
+        :returns: the index of next section else -1
+        """
+        raise NotImplementedError
+
+    def get_next_section_lines(self, data):
+        """Get the starting line number and the ending line number of next section.
+        It will return (-1, -1) if no section was found.
+        The section is a section key (e.g. 'Parameters') then the content
+        The ending line number is the line after the end of the section or -1 if
+        the section is at the end.
+
+        :param data: the data to proceed
+
+        """
+        end = -1
+        start = self.get_next_section_start_line(data)
+        if start != -1:
+            end = self.get_next_section_start_line(data[start + 1:])
+        return start, end
+
+    def get_key_section_header(self, key, spaces):
+        """Get the key of the section header
+
+        :param key: the key name
+        :param spaces: spaces to set at the beginning of the header
+
+        """
+        if key in self.section_headers:
+            header = self.section_headers[key]
+        else:
+            return ''
+
+        return header
+
+    def get_section_key_line(self, data, key, opt_extension=''):
+        """Get the next section line for a given key.
+
+        :param data: the data to proceed
+        :param key: the key
+        :param opt_extension: an optional extension to delimit the opt value
+
+        """
+        start = 0
+        init = 0
+        while start != -1:
+            start = self.get_next_section_start_line(data[init:])
+            init += start
+            if start != -1:
+                if data[init].strip().lower() == self.opt[key] + opt_extension:
+                    break
+                init += 1
+        if start != -1:
+            start = init
+        return start
 
 
 class NumpydocTools(DocToolsBase):
@@ -155,6 +298,11 @@ class NumpydocTools(DocToolsBase):
                                                 'ref': 'references',
                                                 'return': 'returns',
                                             },
+                                            section_headers={
+                                                'param': 'Parameters',
+                                                'return': 'Returns',
+                                                'raise': 'Raises',
+                                            },
                                             )
 
         self.keywords = [
@@ -163,23 +311,6 @@ class NumpydocTools(DocToolsBase):
             'see also',
             '.. image::',
         ]
-
-    def __iter__(self):
-        return self.opt.__iter__()
-
-    def __getitem__(self, key):
-        return self.opt[key]
-
-    def get_optional_sections(self):
-        return self.optional_sections
-
-    def get_excluded_sections(self):
-        return self.excluded_sections
-
-    def get_mandatory_sections(self):
-        return [s for s in self.opt
-                if s not in self.optional_sections and
-                   s not in self.excluded_sections]
 
     def get_next_section_start_line(self, data):
         """Get the starting line number of next section.
@@ -202,43 +333,6 @@ class NumpydocTools(DocToolsBase):
             if isin_alone(self.opt.values(), line):
                 start = i
         return start
-
-    def get_section_key_line(self, data, key):
-        """Get the next section line for a given key.
-
-        :param data: the data to proceed
-        :param key: the key
-
-        """
-        start = 0
-        init = 0
-        while start != -1:
-            start = self.get_next_section_start_line(data[init:])
-            init += start
-            if start != -1:
-                if data[init].strip().lower() == self.opt[key]:
-                    break
-                init += 1
-        if start != -1:
-            start = init
-        return start
-
-    def get_next_section_lines(self, data):
-        """Get the starting line number and the ending line number of next section.
-        It will return (-1, -1) if no section was found.
-        The section is a section key (e.g. 'Parameters') followed by underline
-        (made by -), then the content
-        The ending line number is the line after the end of the section or -1 if
-        the section is at the end.
-
-        :param data: the data to proceed
-
-        """
-        end = -1
-        start = self.get_next_section_start_line(data)
-        if start != -1:
-            end = self.get_next_section_start_line(data[start + 1:])
-        return start, end
 
     def get_list_key(self, data, key):
         """Get the list of a key elements.
@@ -297,47 +391,6 @@ class NumpydocTools(DocToolsBase):
         """
         return self.get_list_key(data, 'attr')
 
-    def get_param_list(self, data):
-        """Get the list of parameters.
-        The list contains tuples (name, desc, type=None)
-
-        :param data: the data to proceed
-
-        """
-        return self.get_list_key(data, 'param')
-
-    def get_return_list(self, data):
-        """Get the list of return elements/values.
-        The list contains tuples (name=None, desc, type)
-
-        :param data: the data to proceed
-
-        """
-        return_list = []
-        lst = self.get_list_key(data, 'return')
-        for l in lst:
-            name, desc, rtype = l
-            if l[2] is None:
-                rtype = l[0]
-                name = None
-            return_list.append((name, desc, rtype))
-        return return_list
-
-    def get_raise_list(self, data):
-        """Get the list of exceptions.
-        The list contains tuples (name, desc)
-
-        :param data: the data to proceed
-
-        """
-        return_list = []
-        lst = self.get_list_key(data, 'raise')
-        for l in lst:
-            # assume raises are only a name and a description
-            name, desc, _ = l
-            return_list.append((name, desc))
-        return return_list
-
     def get_raw_not_managed(self, data):
         """Get elements not managed. They can be used as is.
 
@@ -373,14 +426,7 @@ class NumpydocTools(DocToolsBase):
         :param spaces: spaces to set at the beginning of the header
 
         """
-        if key == 'param':
-            header = 'Parameters'
-        elif key == 'return':
-            header = 'Returns'
-        elif key == 'raise':
-            header = 'Raises'
-        else:
-            return ''
+        header = super(NumpydocTools, self).get_key_section_header(key, spaces)
         header = spaces + header + '\n' + spaces + '-' * len(header) + '\n'
         return header
 
@@ -416,63 +462,22 @@ class GoogledocTools(DocToolsBase):
                                                  'return': 'returns',
                                                  'yield': 'yields',
                                              },
+                                             section_headers={
+                                                 'param': 'Args',
+                                                 'return': 'Returns',
+                                                 'raise': 'Raises',
+                                             },
                                              )
 
-        self.section_headers = {
-            'param': 'Args',
-            'return': 'Returns',
-            'raise': 'Raises',
-        }
-
-    def __iter__(self):
-        return self.opt.__iter__()
-
-    def __getitem__(self, key):
-        return self.opt[key]
-
-    def get_optional_sections(self):
-        """Get optional sections"""
-        return self.optional_sections
-
-    def get_excluded_sections(self):
-        """Get excluded sections"""
-        return self.excluded_sections
-
-    def get_next_section_lines(self, data):
-        """Get the starting line number and the ending line number of next section.
-        It will return (-1, -1) if no section was found.
-        The section is a section key (e.g. 'Parameters') then the content
-        The ending line number is the line after the end of the section or -1 if
-        the section is at the end.
-
-        :param data: the data to proceed
-
-        """
-        end = -1
-        start = self.get_next_section_start_line(data)
-        if start != -1:
-            end = self.get_next_section_start_line(data[start + 1:])
-        return start, end
-
-    def get_section_key_line(self, data, key):
+    def get_section_key_line(self, data, key, opt_extension=':'):
         """Get the next section line for a given key.
 
         :param data: the data to proceed
         :param key: the key
+        :param opt_extension: an optional extension to delimit the opt value
 
         """
-        start = 0
-        init = 0
-        while start != -1:
-            start = self.get_next_section_start_line(data[init:])
-            init += start
-            if start != -1:
-                if data[init].strip().lower() == self.opt[key] + ":":
-                    break
-                init += 1
-        if start != -1:
-            start = init
-        return start
+        return super(GoogledocTools, self).get_section_key_line(data, key, opt_extension)
 
     def get_list_key(self, data, key):
         """Get the list of a key elements.
@@ -541,56 +546,6 @@ class GoogledocTools(DocToolsBase):
 
         return key_list
 
-    def get_param_list(self, data):
-        """Get the list of parameters.
-        The list contains tuples (name, desc, type=None)
-
-        :param data: the data to proceed
-
-        """
-        return self.get_list_key(data, 'param')
-
-    def get_return_list(self, data):
-        """Get the list of returned values.
-        The list contains tuples (name=None, desc, type=None)
-
-        :param data: the data to proceed
-
-        """
-        return_list = []
-        lst = self.get_list_key(data, 'return')
-        for l in lst:
-            name, desc, rtype = l
-            if l[2] is None:
-                rtype = l[0]
-                name = None
-                desc = desc.strip()
-            return_list.append((name, desc, rtype))
-
-        return return_list
-
-    def get_raise_list(self, data):
-        """Get the list of exceptions.
-        The list contains tuples (name, desc)
-
-        :param data: the data to proceed
-
-        """
-        return_list = []
-        lst = self.get_list_key(data, 'raise')
-        for l in lst:
-            # assume raises are only a name and a description
-            name, desc, _ = l
-            return_list.append((name, desc))
-
-        return return_list
-
-    def get_mandatory_sections(self):
-        """Get mandatory sections"""
-        return [s for s in self.opt
-                if s not in self.optional_sections and
-                   s not in self.excluded_sections]
-
     def get_next_section_start_line(self, data):
         """Get the starting line number of next section.
         It will return -1 if no section was found.
@@ -615,10 +570,7 @@ class GoogledocTools(DocToolsBase):
         :param spaces: spaces to set at the beginning of the header
 
         """
-        if key in self.section_headers:
-            header = self.section_headers[key]
-        else:
-            return ''
+        header = super(GoogledocTools, self).get_key_section_header(key, spaces)
         header = spaces + header + ':' + '\n'
         return header
 
