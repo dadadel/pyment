59d24cf23d5cfda943ed93b646d05247f9828199
Adel Daouzli <dadel@hadoly.fr>
2021-02-20 17:46:17 +0100

    update CHANGELOG and setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,8 @@ setup(name='Pyment',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6'
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7'
           ],
       url='https://github.com/dadadel/pyment',
       packages=['pyment'],
