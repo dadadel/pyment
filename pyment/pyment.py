c630fafe4fc5984ce1c3cc7cb4716e2e1da199c0
Adel Daouzli <dadel@hadoly.fr>
2021-03-08 11:49:04 +0100

    fix version 0.3.3 to 0.3.4 and CHANGELOG

--- pyment/pyment.py
+++ pyment/pyment.py
@@ -11,7 +11,7 @@ from .docstring import DocString
 __author__ = "A. Daouzli"
 __copyright__ = "Copyright 2012-2021"
 __licence__ = "GPL3"
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __maintainer__ = "A. Daouzli"
 
 #TODO:
