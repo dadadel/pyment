ade6859e67abb9c7f867e2cd66a95f825799346c
Adel Daouzli <dadel@hadoly.fr>
2021-03-09 14:04:49 +0100

    #85: add support for type declaration in param statement for reST docstrings

---
+++ tests/issue85.py
@@ -0,0 +1,58 @@
+def func(param_file, mask):
+    """
+    :param lxml.etree.ElementTree param_file: xml-element tree
+        obtained from the file.
+    :param str mask: Name of mask to be considered.
+    """
+    pass
+
+
+def func2(param1: str, param2):
+    """
+    :param str param1: description 1
+    :param str param2: description 2
+    :returns: description return
+    """
+    pass
+
+
+def func3(param1: str, param2):
+    """
+    :param param1: description 1 is
+        a multiline description
+    :type param1: str
+    :param str param2: description 2
+    :returns: description return
+    """
+    pass
+
+
+def func4(param1, param2):
+    """
+    :param param1: description 1
+    :type param1: str
+    :param str param2: description 2
+    :returns: description return
+    """
+    pass
+
+
+def func5(param1, param2):
+    """
+    :param int param1: description 1
+    :type param1: str
+    :param param2: description 2
+    :returns: description return
+    """
+    pass
+
+
+def func6(param1: list, param2):
+    """
+    :param int param1: description 1
+    :type param1: str
+    :param param2: description 2
+    :returns: description return
+    """
+    pass
+
