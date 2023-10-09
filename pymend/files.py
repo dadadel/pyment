8c9a63b781587fa0ddf12f735b3454a4f904a435
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2023-10-09 17:04:53 +0200

    Pretty sure this causes and error for 11 and 12 again

--- pymend/files.py
+++ pymend/files.py
@@ -85,10 +85,6 @@ def parse_pyproject_toml(path_config: str) -> dict[str, Any]:
     If parsing fails, will raise a tomllib.TOMLDecodeError.
     """
     with open(path_config, "rb") as f:
-        pyproject_toml: dict[
-            str, Any
-        ] = tomllib.load(  # pyright: ignore[reportUnboundVariable]
-            f
-        )
+        pyproject_toml: dict[str, Any] = tomllib.load(f)
     config: dict[str, Any] = pyproject_toml.get("tool", {}).get("pymend", {})
     return {k.replace("--", "").replace("-", "_"): v for k, v in config.items()}
