4fedeae4afae95916a9a055df0499019605936a5
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2023-10-12 17:34:48 +0200

    Update version

--- pymend/pymend.py
+++ pymend/pymend.py
@@ -22,7 +22,7 @@ from .types import ElementDocstring, FixerSettings
 __author__ = "J-E. Nitschke"
 __copyright__ = "Copyright 2012-2021 A. Daouzli"
 __licence__ = "GPL3"
-__version__ = "1.0.9"
+__version__ = "1.0.10"
 __maintainer__ = "J-E. Nitschke"
 
 
